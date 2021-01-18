9a39d6bc91e3069136ac2add876fa33a97b7e333
DiegoBracci <dbraccipro@gmail.com>
2021-01-18 19:51:04 +0000

    Final TodoList Sin Fecht

---
+++ src/js/component/TodoList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+export const TodoList = props => {
+	const [todoList, setTodoList] = useState([]);
+	let [newTask, setNewTask] = useState("");
+
+	const handleTask = () => {
+		if (event.key === "Enter" && newTask != "") {
+			setTodoList(todoList.concat(newTask));
+			setNewTask("");
+		}
+	};
+
+	const deleteTask = value => {
+		setTodoList(todoList.filter(task => task !== value));
+	};
+
+	return (
+		<div>
+			<input
+				value={newTask}
+				placeholder=" Your List here !"
+				onChange={e => setNewTask(e.target.value)}
+				onKeyPress={e => handleTask(e)}
+			/>
+			<ul>
+				{todoList.map((item, index) => (
+					<li key={index}>
+						{item}
+						<label
+							onClick={() => {
+								deleteTask(item);
+							}}>
+							x
+						</label>
+					</li>
+				))}
+			</ul>
+			<label className="numberList">{todoList.length} Tasks</label>
+		</div>
+	);
+};
